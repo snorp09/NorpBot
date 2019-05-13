a9cb6e2f29b0882bf48ab432d2a39c48ee907f2a
Snorp <snorp08@gmail.com>
2019-05-13 01:39:26 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+const env = require('dotenv').config();
+const Discord = require("discord.js");
+const client = new Discord.Client;
+const Guild = new Discord.Guild;
+
+client.on('message', msg => {
+    if (msg.content === "!norp") {
+        msg.reply(" Norp.")
+    }
+    if (msg.content === "!NE" && msg.author.username === "Snorp09") {
+        console.log(process.env.TEST)
+    }
+    msg.guild.members.get("559819265872756749").setNickname("NorpBot")
+})
+
+client.login(process.env.BOT_TOKEN)
