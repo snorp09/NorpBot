0da0bdf286832014d82771d84921189463cfb8ae
Snorp09 <snorp08@gmail.com>
2019-09-11 11:59:40 -0700

    Added !offline command for stopping bot in chat. Needs more messages for each type of error regarding permissions still.

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ const Guild = new Discord.Guild;
 
 client.on('message', msg => {
     if (msg.content === "!norp" || msg.content === "!Norp") {
-        msg.reply(" Norp.")
+        msg.reply(" Norp.");
     }
     if (msg.content ==="!source" || msg.content === "!Source" ){
         msg.author.send("A link to the Norpbot source code: http://www.github.com/snorp09/NorpBot")
     }
-})
+    try{
+        if(msg.content === "!offline" && msg.member.hasPermission("administrator")){
+            client.destroy();
+        }
+    }
+    //Catches all error regarding permissions. TODO Add different messages for each type of errors
+    catch (error) {
+        console.log("Caught Error: " + error);
+        msg.reply("Error while running '!offline' command. Are you in DMs?");
+    }
+});
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
