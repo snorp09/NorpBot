d66278de1895e379c3236016168cde37617e5fcb
snorp09 <snorp08@gmail.com>
2019-10-31 22:59:25 -0700

    Add spam protection to the !norp command, and I've added different error messages, responding to each type of error that will regularly occur with the offline command.
    Signed-off-by: snorp09 <snorp08@gmail.com>

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ const Discord = require("discord.js");
 const client = new Discord.Client;
 //const Guild = new Discord.Guild;
 
+let norpReady = true;
+
 client.on('message', msg => {
+
+    //The core of the bot. The Norp command, with timeout for spam control.
     if (msg.content === "!norp" || msg.content === "!Norp") {
-        msg.reply(" Norp.");
+        if(norpReady === true){
+            msg.reply("Norp.");
+            norpReady = false;
+            setTimeout(function () {
+                console.log("Wait done. Enabling Norp.");
+                norpReady = true;
+            }, 5000)
+        }
+        else{
+            console.log("Norp not ready.");
+            console.log(norpReady)
+        }
     }
     if (msg.content ==="!source" || msg.content === "!Source" ){
         msg.author.send("A link to the Norpbot source code: http://www.github.com/snorp09/NorpBot")
     }
+
+    //!Offline preforms a clean shutdown of the bot. Requires Administrator permissions.
     try{
         if(msg.content === "!offline" && msg.member.hasPermission("administrator")){
             client.destroy().then( function () {
@@ -18,10 +35,19 @@ client.on('message', msg => {
             });
         }
     }
-    //Catches all error regarding permissions. TODO Add different messages for each type of errors
+    //Catches all error regarding permissions.
     catch (error) {
         console.log("Caught Error: " + error);
-        msg.reply("Error while running '!offline' command. Are you in DMs?");
+        if(error instanceof RangeError){
+            msg.reply("An error has occurred. This type is usually cased by wrong permissions. Are you admin? ")
+        }
+        else if(error instanceof TypeError){
+            msg.channel.send("An error has occurred. This type is usually caused by trying to set the bot offline in DMs. Try outside of DMs.")
+        }
+        else{
+            msg.reply("An unknown type of error has occurred. Please check your node.js console for details, and possibly make an issue request at https://github.com/snorp09/NorpBot/issues");
+            msg.channel.send("They'll want have a copy of the error you've received in your node.js console, if you opt to make a issue.")
+        }
     }
 });
 
